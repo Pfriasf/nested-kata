3c86b52113d7994bcd414fa2f175725f23003416
Pedro Frias <plfriasf@gmail.com>
2021-04-28 14:43:55 -0700

    First expect red light 🔴

---
+++ test/alphabetPosition.test.js
@@ -0,0 +1,8 @@
+const alphabetPosition = require("../js/alphabetposition")
+
+describe('Alphabetic position function', () => {
+
+    it('Should be a function', () => {
+        expect(typeof alphabetPosition).toEqual("function")
+    })
+})
