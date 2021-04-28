1638e9eeaea8fec3bf01ae5a44efddbe8108bf8e
Pedro Frias <plfriasf@gmail.com>
2021-04-28 14:50:43 -0700

    Second  expect red light 🔴

--- test/alphabetPosition.test.js
+++ test/alphabetPosition.test.js
@@ -5,4 +5,10 @@ describe('Alphabetic position function', () => {
     it('Should be a function', () => {
         expect(typeof alphabetPosition).toEqual("function")
     })
+
+    it("Should return every letter with its position in the alphabet", () => {
+      expect(alphabetPosition('abcd')).toEqual("1 2 3");
+    });
+
+
 })
