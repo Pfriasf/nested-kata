adb041077b2b1399eed30ae0f6be97ccb31f433d
Pedro Frias <plfriasf@gmail.com>
2021-04-28 06:13:26 -0700

    Second expect red  light 🔴

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
+/*Compression
+Write a function that takes a string as an argument and generates an array that sums all the repeating characters.
+
+Test Cases
+compress('') // []
+compress('abcd') // [[1, 'a'], [1, 'b'], [1, 'c'], [1, 'd']]
+compress('aaaaaaaabaaaa') // [[8,'a'], [1,'b'], [4,'a']]
+*/
+
 const generateArray = (str) => {
     if (str === "") {
         return []
     }
+
+  
 }
 
 module.exports = generateArray
--- test/generatteArray.test.js
+++ test/generatteArray.test.js
@@ -1,9 +1,13 @@
 const generateArray = require("..")
 
-describe('generate Array function', () => {
+describe('Generate Array function', () => {
 
     it('Should return [] if the input is a empty string', () => {
         
         expect(generateArray("")).toEqual([])
     })
+
+    it("Should return an array that sums all the repeating characters", () => {
+      expect(generateArray("abcd")).toEqual([[1, 'a'], [1, 'b'], [1, 'c'], [1, 'd']]);
+    });
 })
