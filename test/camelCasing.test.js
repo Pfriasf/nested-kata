5db5fe39a113af927518b6151b4743e0c48db5a8
Pedro Frias <plfriasf@gmail.com>
2021-04-28 08:29:00 -0700

    Second and third expect red light 🔴

--- test/camelCasing.test.js
+++ test/camelCasing.test.js
@@ -7,8 +7,23 @@ describe('Camel casing function', () => {
     })
     
     it('Should return "" if the input is a empty string', () => {
+
         expect(camelCasing("")).toBe("")
+
     })
 
-    
+    it("Should break up camel casing, using a space between words", () => {
+
+      expect(camelCasing("camelCasing")).toBe("camel Casing");
+
+    });
+
+
+    it("Should break up camel casing, using a space between words even when there are more than two words", () => {
+      
+        expect(camelCasing("camelCasingTest")).toBe("camel Casing Test");
+        
+    });
+
+
 })
